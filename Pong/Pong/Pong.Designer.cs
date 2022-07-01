be42b17db8e72d262af2c9bc71157eda7253237a
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-01 14:23:46 +0200

    works now

--- Pong/Pong/Pong.Designer.cs
+++ Pong/Pong/Pong.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pong));
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.LightCyan;
+            this.player1.Location = new System.Drawing.Point(83, 593);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(36, 294);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.Player2.Location = new System.Drawing.Point(1877, 593);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(36, 294);
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(715, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 142);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Pong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2825, 1853);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1966, 1576);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.player1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pong";
+            this.Text = "Play pong";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox player1;
+        private PictureBox Player2;
+        private PictureBox pictureBox1;
     }
 }
